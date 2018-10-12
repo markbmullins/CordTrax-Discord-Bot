c2ebfac5aee7500ac2bd21f75ae4faa118ed8dbd
markbmullins <markmullins95@gmail.com>
2018-10-11 22:47:26 -0400

    Created myqueues command

--- commands/functions/queueFunctions.js
+++ commands/functions/queueFunctions.js
@@ -65,6 +65,32 @@ module.exports = {
 			}//end function
 		);//end promise
 	},//end getQueue();
+	getAllQueues: async function getAllQueues(message, con_database){
+		return new Promise(
+			(resolve, reject) => {
+				con_database.query(`SELECT * FROM queues WHERE userid = '${message.author.id}'`, 
+					(err, result) =>{
+						if(err) reject(err);
+						else{
+							if(!result.rows[0]){
+								let queues = [];
+								resolve(queues);
+							}//end if
+							else{
+								let queues = [];
+								let i = 0;
+								result.rows.forEach(element=>{
+									queues[i] = result.rows[i].queuename;
+									i++;
+								});
+								resolve(queues);
+							}//end else
+						}//end else
+					}//end (err,result) =>
+				);//end con_datbase.query
+			}//end function
+		);//end promise
+	},//end getQueue();
 	getURL: async function getURL(message, song){
 			var url;
 			try{
