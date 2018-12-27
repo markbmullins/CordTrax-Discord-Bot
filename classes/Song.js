2f1caecf1179605f84e7c8ff81e618562281740e
Mark Mullins <markmullins95@gmail.com>
2018-12-27 15:21:07 -0500

    Update Song.js

--- classes/Song.js
+++ classes/Song.js
@@ -1,1 +1,22 @@
+//Song Class
 
+class Song{
+		constructor(title, url){
+		    this._title = title;
+			this._URL = url;
+		}
+		get title(){
+			return this._title;
+		}
+		get URL(){
+			return this._URL;
+		}
+		updateURL(message){
+			this._URL = this.getUrl(message,this._title);
+		}
+		updateTitle(){
+			return;
+		}
+}
+
+module.exports = Song;
