bb4f17e7ce7233670482eee245a651e55b29c93d
Mark Mullins <markmullins95@gmail.com>
2018-12-28 10:59:54 -0500

    Updated path to helpMessages.json

--- commands/addrole.js
+++ commands/addrole.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const helpMessages = require("./functions/helpMessages.json");
+const helpMessages = require('../helpMessages.json');
 
 module.exports.run = async (client,message,args,prefix,con_database) => {
 	//!addrole @user <role(case sensitive)>
